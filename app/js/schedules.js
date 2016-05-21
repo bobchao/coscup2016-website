2d80b24385e8da64778492d81db690b6fd001f67
Sun mokoto <s2821d3721@gmail.com>
2016-05-21 10:30:16 +0800

    Update# add footer sponsor to each page

--- app/js/schedules.js
+++ app/js/schedules.js
@@ -3,16 +3,18 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 
 // Include dependency
-var Navbar = require('components/navbar.js');
-var Footer = require('components/footer.js');
+var Navbar   = require('components/navbar.js');
+var Sponsors = require('components/footer-sponsor.js');
+var Footer   = require('components/footer.js');
 
 // Implement index page
 var Main = React.createClass({
     render: function() {
         return (
             <div>
-                <Navbar />
-                <Footer />
+                <Navbar   />
+                <Sponsors />
+                <Footer   />
             </div>
         );
     }
--- app/js/transport.js
+++ app/js/transport.js
@@ -5,6 +5,7 @@ var ReactDOM  = require('react-dom');
 // Include dependency
 var Navbar       = require('components/navbar.js');
 var TransportMap = require('components/transportmap.js')
+var Sponsors = require('components/footer-sponsor.js');
 var Footer       = require('components/footer.js');;
 
 // Implement index page
@@ -12,9 +13,10 @@ var Main = React.createClass({
     render: function() {
         return (
             <div>
-                <Navbar />
+                <Navbar       />
                 <TransportMap />
-                <Footer />
+                <Sponsors     />
+                <Footer       />
             </div>
         );
     }
