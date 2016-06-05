11eb8c62263345f76083d6d2555f24041847e0d8
Sun mokoto <s2821d3721@gmail.com>
2016-06-05 16:08:30 +0800

    Update live page(ASAP)

--- app/js/live.js
+++ app/js/live.js
@@ -3,18 +3,25 @@ var React     = require('react');
 var ReactDOM  = require('react-dom');
 
 // Include dependency
-var Navbar   = require('components/navbar.js');
-var Sponsors = require('components/footer-sponsor.js');
-var Footer   = require('components/footer.js');
+var Navbar    = require('components/navbar.js');
+var Seperator = require('components/seperator.js');
+var Sponsors  = require('components/footer-sponsor.js');
+var Footer    = require('components/footer.js');
 
 // Implement index page
 var Main = React.createClass({
     render: function() {
         return (
             <div>
-                <Navbar   />
+                <Navbar />
+
+                <strong role="ASAP">ASAP</strong>
+
+                <Seperator />
+
                 <Sponsors />
-                <Footer   />
+
+                <Footer />
             </div>
         );
     }
