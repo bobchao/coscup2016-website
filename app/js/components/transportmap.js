ab330d552f686b8b95cdb5e8fd630e3b9936063b
Tocknicsu <wingemerald@gmail.com>
2016-06-05 16:53:27 +0800

    UPDATE transport scss

--- app/js/components/transportmap.js
+++ app/js/components/transportmap.js
@@ -21,30 +21,28 @@ var TransportMap = React.createClass({
         var lang = this.state.lang;
         var lis  = TransportJsonDt.transport.map(function(transport, id) {
             return (
-                <section>
-                    <h3>{transport.title[lang]}</h3>
-                    <div>{transport.content[lang]}</div>
+                <section role="transport">
+                    <h3 role="title">{transport.title[lang]}</h3>
+                    <div role="content">{transport.content[lang]}</div>
                 </section>
             );
         });
         return (
             <div>
                 <div>
-                    <iframe width="425" height="350" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" 
-                        src="http://www.openstreetmap.org/export/embed.html?bbox=121.60438299179079%2C25.0365578919576%2C121.6185450553894%2C25.045461759833863&amp;layer=mapnik&amp;marker=25.041009906698907%2C121.61146402359009" >
-                    </iframe>
+                    <iframe width="100%" height="600px" frameBorder="0" src="http://umap.openstreetmap.fr/zh-tw/map/ubike_88883?scaleControl=true&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false&datalayers=209688#15/25.0493/121.6126"></iframe>
                 </div>
                 <div>
-                    <section>
-                        <h3>{TransportJsonDt.location.title[lang]}</h3>
-                        <div>{TransportJsonDt.location.content[lang]}</div>
+                    <section role="transport">
+                        <h3 role="title">{TransportJsonDt.location.title[lang]}</h3>
+                        <div role="content">{TransportJsonDt.location.content[lang]}</div>
                     </section>
-                    <section>
-                        <h3>{TransportJsonDt.address.title[lang]}</h3>
-                        <div>{TransportJsonDt.address.content[lang]}</div>
+                    <section role="transport">
+                        <h3 role="title">{TransportJsonDt.address.title[lang]}</h3>
+                        <div role="content">{TransportJsonDt.address.content[lang]}</div>
                     </section>
-                    <section>
-                        <h3>{TransportJsonDt.suggestion[lang]}</h3>
+                    <section role="transport">
+                        <h3 role="title">{TransportJsonDt.suggestion[lang]}</h3>
                     </section>
                     {lis}
                 </div>
