77e308cde8cfddc66196fb5994739ba3212d93b2
Sun mokoto <s2821d3721@gmail.com>
2016-06-06 09:05:06 +0800

    Fix build bug

--- gulp/js.js
+++ gulp/js.js
@@ -2,31 +2,37 @@ var errorLog = require('./_errorLog.js').errorLog;
 var jsPath   = require('./_config.js').js;
 
 var gulp       = require('gulp');
-var uglify     = require('gulp-uglify');
+var uglify     = require('gulp-uglify')
 var browserify = require('gulp-browserify');
 var reactify   = require('reactify');
+var babel      = require('gulp-babel');;
 
 gulp.task('js', function(){
 
-	process.env.NODE_ENV = 'production';
-	
-	return gulp.src(jsPath.src)
-		.pipe(browserify({
-			transform: ['reactify'],
-			paths: jsPath.include_paths
-		}))
-		.on('error', errorLog)
-		.pipe(uglify())
-		.pipe(gulp.dest(jsPath.dst));
+    process.env.NODE_ENV = 'production';
+    
+    return gulp.src(jsPath.src)
+        .pipe(browserify({
+            transform: ['reactify'],
+            paths: jsPath.include_paths
+        }))
+        .on('error', errorLog)
+        .pipe(babel({
+            presets: ['react', 'es2015']
+        }))
+        .on('error', errorLog)
+        .pipe(uglify())
+        .on('error', errorLog)
+        .pipe(gulp.dest(jsPath.dst));
 });
 
 gulp.task('js-debug', function(){
-	return gulp.src(jsPath.src)
-		.pipe(browserify({
-			transform: ['reactify'],
-			debug: true,
-			paths: jsPath.include_paths
-		}))
-		.on('error', errorLog)
-		.pipe(gulp.dest(jsPath.dst));
+    return gulp.src(jsPath.src)
+        .pipe(browserify({
+            transform: ['reactify'],
+            debug: true,
+            paths: jsPath.include_paths
+        }))
+        .on('error', errorLog)
+        .pipe(gulp.dest(jsPath.dst));
 });
