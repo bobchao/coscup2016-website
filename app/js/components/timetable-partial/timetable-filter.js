c9ba8584ab457ecfb86606565058e233627d0033
Sun mokoto <s2821d3721@gmail.com>
2016-06-09 10:31:54 +0800

    Add schedule filter

--- app/js/components/timetable-partial/timetable-filter.js
+++ app/js/components/timetable-partial/timetable-filter.js
@@ -1,11 +1,99 @@
 // Include library
 var React     = require('react');
 
+// Include dependency
+var langStore = require('stores/lang.js');
+var typeStore = require('stores/timetable-filter.js');
+
 // Implement index page
-var TimetableFilter = React.createClass({
+var PlaceBtn = React.createClass({
+    needActive: function() {
+        return typeStore.nowPlace()==this.props.data.id;
+    },
+    getInitialState: function() {
+        return {active: this.needActive()};
+    },
+    changeHandler: function(nowPlace) {
+        this.setState({active: this.needActive()});
+    },
+    componentDidMount: function() {
+        typeStore.placeChangeRegister(this.changeHandler);
+    },
+    clickHandler: function() {
+        typeStore.setPlace(this.props.data.id);
+    },
     render: function() {
+        var data = this.props.data;
+        var lang = this.props.lang;
+        var cls  = (this.state.active)? 'active' : '';
         return (
-            <div role="timetable-filter"></div>
+            <button onClick={this.clickHandler} className={cls}>
+                {data[lang]}
+            </button>
+        );
+    }
+});
+
+var FilterBtn = React.createClass({
+    needActive: function() {
+        return typeStore.sieve(this.props.data.type);
+    },
+    getInitialState: function() {
+        return {active: this.needActive()};
+    },
+    changeHandler: function(nowPlace) {
+        if( this.state.active != this.needActive() )
+            this.setState({active: this.needActive()});
+    },
+    componentDidMount: function() {
+        typeStore.filterChangeRegister(this.changeHandler);
+    },
+    clickHandler: function() {
+        typeStore.filterToggle(this.props.data.type);
+    },
+    render: function() {
+        var data = this.props.data;
+        var lang = this.props.lang;
+        var clk  = (this.state.active)? 'active' : '';
+        return (
+            <span onClick={this.clickHandler} className={clk}>
+                <div role="light-box"></div>
+                {data['name'+lang]}
+            </span>
+        );
+    }
+});
+
+var TimetableFilter = React.createClass({
+    getInitialState: function() {
+        return {lang: langStore.getState()};
+    },
+    changeHandler: function() {
+        this.setState({lang: langStore.getState()});
+    },
+    componentDidMount: function() {
+        langStore.register(this.changeHandler);
+    },
+    render: function() {
+        var lang  = this.state.lang;
+        var place = typeStore.getAllPlaces();
+        var tags  = [];
+        var types = typeStore.getAllTypes();
+        for(var id in types)
+            tags.push(
+                <FilterBtn key={lang+id} data={types[id]} lang={lang} />
+            );
+        return (
+            <div role="timetable-filter">
+                <header>
+                    <PlaceBtn role="left-btn" data={place[0]} lang={lang} />
+                    <PlaceBtn role="right-btn" data={place[1]} lang={lang} />
+                    <div role="clear-float"></div>
+                </header>
+                <footer>
+                    {tags}
+                </footer>
+            </div>
         );
     }
 });
--- app/js/stores/timetable-filter.js
+++ app/js/stores/timetable-filter.js
@@ -1,35 +1,79 @@
-// Include library
-var assign       = require('object-assign');
-var EventEmitter = require('events').EventEmitter;
-
 // map id to tag info
 var typesData = require('json/type.json');
-var types    = [];
+var types     = {};
 typesData.forEach((ele) => {
     types[ele.type] = ele;
 });
+var placesData = [
+    {id: 0, en: "Academia Sinica", zh: "人文館"},
+    {id: 1, en: "Activity Center", zh: "活動中心"}
+];
 
-var sieve  = [];
+// sieve, cnt do the same thing as Set
+var sieve  = {};
 var cnt    = 0;
+var placeid= 0;
 
-var store = assign({}, EventEmitter.prototype, {
+var filterChangeCallbacks = [];
+var placeChangeCallbacks  = [];
+
+var store = {
+    getAllTypes: function() {
+        return types;
+    }, 
+    getAllPlaces: function() {
+        return placesData;
+    },
+    // Return whether a filter was picked or not
     filter: function(id) {
         if( cnt==0 ) return true;
         if( typeof sieve[id] === 'undefined' )
             return false;
         return sieve[id];
     },
+    sieve : function(id) {
+        if( sieve[id] ) return true;
+        else return false
+    },
+    // Map id to tag info
     type: function(id) {
         return types[id];
     },
-
-
-    register: function(callback) {
-        this.on('change', callback);
+    nowPlace: function() {
+        return placeid;
     },
-    unregister: function(callback) {
-        this.removeListener('change', callback);
+    filterToggle: function(id) {
+        if( sieve[id] ) {
+            sieve[id] = false;
+            --cnt;
+        }
+        else {
+            sieve[id] = true;
+            ++cnt;
+        }
+        this.emitFilterChange();
+    },
+    setPlace: function(val) {
+        if( placeid!=val ) {
+            placeid = val;
+            this.emitPlaceChange();
+        }
+    },
+
+    filterChangeRegister: function(callback) {
+        filterChangeCallbacks.push(callback);
+    },
+    placeChangeRegister: function(callback) {
+        placeChangeCallbacks .push(callback);
+    },
+    emitFilterChange: function() {
+        for(var callback of filterChangeCallbacks)
+            callback();
+    },
+    emitPlaceChange: function() {
+        for(var callback of placeChangeCallbacks)
+            callback(placeid);
     }
-});
+};
 
 module.exports = store;
