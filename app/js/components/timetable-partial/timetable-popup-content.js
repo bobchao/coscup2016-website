06912599763f01a81568f81280df0429826bafba
Sun mokoto <s2821d3721@gmail.com>
2016-06-10 10:02:02 +0800

    Update schedulet speakerintro markdown

--- app/js/components/timetable-partial/timetable-popup-content.js
+++ app/js/components/timetable-partial/timetable-popup-content.js
@@ -1,13 +1,21 @@
 var React     = require('react');
+var Remarkable= require('remarkable');
+
+function markup(text) {
+    var md       = new Remarkable({html: true});
+    var markdown = md.render(text);
+    return { __html: markdown};
+}
 
 var Content = React.createClass({
     render: function() {
         var data = this.props.data || {};
+        var speakerintro = markup(data.speakerintro || '');
         return (
             <section role="timetable-popup-content">
                 <header>
                     <strong>{data.speakername}</strong>
-                    <article>{data.speakerintro}</article>
+                    <article dangerouslySetInnerHTML={speakerintro} />
                 </header>
                 <footer>
                     <strong>{data.subject}</strong>
