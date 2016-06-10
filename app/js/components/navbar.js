25a3f2baa95e461247b51f60fdf7f0abbd211aec
Sun mokoto <s2821d3721@gmail.com>
2016-06-10 10:20:02 +0800

    Update index rwd

--- app/js/components/navbar.js
+++ app/js/components/navbar.js
@@ -94,12 +94,12 @@ var Navbar = React.createClass({
         };
         if( resizer.windowWidth() > 1500 )
             nState.height = (resizer.windowWidth()*0.3) + 'px';
-        else if( resizer.windowWidth() > 900 ) {
+        else if( resizer.windowWidth() > 600 ) {
             nState.height = (resizer.windowWidth()*0.45) + 'px';
             nState.cName  = 'banner-rwd-1500';
         }
         else {
-            nState.height = (resizer.windowWidth()*0.45) + 'px';
+            nState.height = (resizer.windowWidth()*0.7) + 'px';
             nState.cName  = 'banner-rwd-900';
         }
         return nState;
